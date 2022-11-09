5e743666a5a153fee3ed6ad554a512958551b5eb
Matthew Nos <mnos@cloudingenuity.com>
2022-11-09 13:57:58 -0600

    First Commit

---
+++ Sources/CloudRTLS.xcframework/ios-arm64_x86_64-maccatalyst/CloudRTLS.framework/Versions/A/Headers/CloudRTLS.h
@@ -0,0 +1,18 @@
+//
+//  CloudRTLS.h
+//  CloudRTLS
+//
+//  Created by Matthew Nos on 11/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CloudRTLS.
+FOUNDATION_EXPORT double CloudRTLSVersionNumber;
+
+//! Project version string for CloudRTLS.
+FOUNDATION_EXPORT const unsigned char CloudRTLSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudRTLS/PublicHeader.h>
+
+
--- Tests/CloudRTLSTests/CloudRTLSTests.swift
+++ Tests/CloudRTLSTests/CloudRTLSTests.swift
@@ -6,6 +6,6 @@ final class CloudRTLSTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CloudRTLS().text, "Hello, World!")
+        
     }
 }
